6dcf52df141e64a2c73baf65aae366bfbab82d55
Arbalest1337 <499624095@qq.com>
2024-12-19 09:24:57 +0800

    feat: init

---
+++ main.js
@@ -0,0 +1,45 @@
+import 'dotenv/config'
+import express from 'express'
+import * as openai from './openai.js'
+
+const PORT = 6657
+const app = express()
+app.use(express.json())
+
+const GlobalMiddleware = (req, res, next) => {
+  console.log(new Date().toLocaleString(), req.method, req.url, req.body)
+  next()
+}
+app.use(GlobalMiddleware)
+
+app.post('/openai/chat/completions', async (req, res) => {
+  try {
+    const res = await openai.chatCompletions(req.body)
+    res.send(success(response))
+  } catch (err) {
+    res.send(failed(err))
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}`)
+})
+
+const success = (data = {}) => {
+  return {
+    code: 200,
+    success: true,
+    message: 'success',
+    data
+  }
+}
+
+const failed = (err = {}) => {
+  console.log(err)
+  return {
+    code: 500,
+    success: false,
+    message: 'failed',
+    data: err
+  }
+}
---
+++ src/openai.js
@@ -0,0 +1,10 @@
+import 'dotenv/config'
+import OpenAI from 'openai'
+
+const apiKey = process.env.OPENAI_KEY
+const openai = new OpenAI({ apiKey })
+
+export const chatCompletions = async config => {
+  const res = await openai.chat.completions.create(config)
+  return res.choices[0].message
+}
